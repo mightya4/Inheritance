5a4aacd88e20584b4df710351d362ab790239a4a
Akeem <akeem.mighty@gmail.com>
2022-11-02 23:59:27 -0400

    Created Vehicle Class + Created Attributes For Vehicle Class + Created ToString method For Vehicle Class

--- Program.java
+++ Program.java
@@ -1,5 +1,35 @@
+class Vehicle {
+
+    public int speed;
+    public int wheels;
+    public String color;
+
+    public Vehicle(int speed, int wheels, String color)
+    {
+        this.speed = speed;
+        this.wheels = wheels;
+        this.color = color;
+    }
+
+    public void applyBrake(int decrement)
+    {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment)
+    {
+        speed += increment;
+    }
+
+    public String toString()
+    {
+        return (String.format("Color is %s | No of wheels on vehicle: %s | Speed is %s", color, wheels, speed));
+    }
+}
+
 class Program {
     public static void main(String[] args) {
-        
+        Vehicle v1 = new Vehicle(0, 0, null);
+        System.out.println(v1.toString());
     }
 }
