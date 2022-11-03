ebd56700f1aec5da17d30e96109e77a9118f2a63
Akeem <akeem.mighty@gmail.com>
2022-11-03 14:06:05 -0400

    Created Motorcycle Class + Created Enum Types

--- Program.java
+++ Program.java
@@ -1,18 +1,48 @@
+enum Type {
+    UNKNOWN("This is an Unknown Vehicle"),
+    MOTORCYCLE("This vehicle is a Motorcycle."),
+    TRUCK("This vehicle is a Truck."),
+    SEDAN("This vehicle is a Sedan."),
+    SPORTS_CAR("This vehicle is a Sports Car"),
+    SUV("This vehicle is a SUV"),
+    COUPE("This vehicle is a Coupe");
+
+    String desc;
+
+    Type(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
+
 class Vehicle {
 
     public int speed;
     public int wheels;
     public String color;
+    public String model;
+    Type type;
 
     public Vehicle()
     {
+        this.type = Type.UNKNOWN;
         this.speed = 0;
         this.wheels = 0;
+        this.model = "Generic";
         this.color = "Factory Silver";
     }
 
-    public Vehicle(int speed, int wheels, String color)
+    public Vehicle(int speed, int wheels, String color, String model, Type type)
     {
+        this.type = type;
+        this.model = model;
         this.speed = speed;
         this.wheels = wheels;
         this.color = color;
@@ -30,17 +60,30 @@ class Vehicle {
 
     public String toString()
     {
-        return (String.format("Color is %s | No of wheels on vehicle: %s | Speed is %s", color, wheels, speed));
+        return (String.format("%s | Model is %s | Color is %s | No of wheels on vehicle: %s | Speed is %s", type.desc, model, color, wheels, speed));
     }
 }
 
 class Motorcycle extends Vehicle{
 
+    public Motorcycle(){
+        this.wheels = 2;
+        this.type = type.MOTORCYCLE;
+    }
+    public Motorcycle(int wheels, int speed, String color, String model) {
+        this.type = type.MOTORCYCLE;
+        this.wheels = wheels;
+        this.speed = speed;
+        this.model = model;
+        this.color = color;
+    }
 }
 
 class Program {
     public static void main(String[] args) {
-        Vehicle v1 = new Vehicle(0, 0, null);
+        Vehicle v1 = new Vehicle();
         System.out.println(v1.toString());
+        Motorcycle m1 = new Motorcycle();
+        System.out.println(m1.toString());
     }
 }
